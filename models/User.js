d773aed911e4f1d9c155b25a6d48c2b0983a3a68
(Tilakshelke08) <(Tilakshelke123@gmail.com)>
2025-04-04 23:56:30 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const  mongoose = require("mongoose");
+
+const userScheme = new mongoose.Schema({
+ firstName: {
+   type : String,
+   required : true,
+   trim: true,
+ },
+ lastName: {
+   type : String,
+   required : true,
+   trim: true,
+ },
+ email: {
+   type : String,
+   required : true,
+   trim: true,
+ },
+ password: {
+   type : String,
+   required : true,
+ },
+ accountType: {
+   type : String,
+   enum:["Admin","Student","Instructor"],
+   required : true,
+ },
+ additionalDetails: {
+   type : mongoose.Schema.Types.ObjectId,
+   required : true,
+   ref:"Profile",
+ },
+ courses: [{
+   type : mongoose.Schema.Types.ObjectId,
+   required : true,
+   ref:"Course",
+ }],
+ image: {
+   type : String,
+   required : true,
+ },
+ courseProgress: [{
+   type : mongoose.Schema.Types.ObjectId,
+   required : true,
+   ref:"CourseProogrss"
+ }],
+});
+module.exports = mongoose.model("User",userScheme); 
+
